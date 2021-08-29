b796aee97f8830ac5439d56769c1519e51fa0235
sid-player <siddhantjain.cer18@itbhu.ac.in>
2021-08-30 01:24:36 +0530

    RegisterView

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-
+from rest_framework.views import APIView
+from .views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +10,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/',RegistrationAPIView.as_view(),name='register')
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import *
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from api.serializer import *
+from rest_framework.response import Response
 from rest_framework.views import APIView
-
 # Create your views here.
+class RegistrationAPIView(APIView):
+    serializer_class=RegistrationAPISerializer
+    def post(self, req):
+        ser=RegistrationAPISerializer(data=req.data)
+        if ser.is_valid():
+            usr_obj = User()
+            usr_obj.username = req.data.get("username")
+            usr_obj.email = req.data.get("email")
+            usr_obj.set_password(req.data.get("password"))
+            usr_obj.save()
+            refresh = RefreshToken.for_user(usr_obj)
+            resp_data= {
+                'access': str(refresh.access_token)
+            }
+            return Response(resp_data,status=status.HTTP_201_CREATED)
+        return Response(ser.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
